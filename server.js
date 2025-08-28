2f530c7dff99f533af611b2db92fe32451ab5fd8
ragetraxx <93855827+ragetraxx@users.noreply.github.com>
2025-08-28 12:23:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,49 +8,43 @@ const BASE_URL = "http://143.44.136.110:6610";
 // Only allow this origin
 const allowedOrigin = "https://ragetb.onrender.com";
 
-// Strict CORS configuration
+// CORS middleware
 app.use(cors({
-origin: function (origin, callback) {
-if (!origin || origin === allowedOrigin) {
-callback(null, true);
-} else {
-callback(new Error("Not allowed by CORS"));
-}
-}
+  origin: function (origin, callback) {
+    if (!origin || origin === allowedOrigin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
 }));
 
 // Set CORS headers manually for streaming
 app.use((req, res, next) => {
-const origin = req.headers.origin;
-if (origin === allowedOrigin) {
-res.setHeader("Access-Control-Allow-Origin", origin);
-} else {
-res.status(403).send("Access denied");
-return;
-}
-res.setHeader("Access-Control-Allow-Headers", "*");
-next();
+  const origin = req.headers.origin;
+  if (!origin || origin === allowedOrigin) {
+    res.setHeader("Access-Control-Allow-Origin", origin || "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+  } else {
+    res.status(403).send("Access denied");
+  }
 });
 
 // Proxy all GET requests
 app.get("/*", (req, res) => {
-const origin = req.headers.origin;
-
-if (origin !== allowedOrigin) {
-return res.status(403).send("Access denied");
-}
-
-const targetUrl = BASE_URL + req.originalUrl;
-
-req.pipe(request(targetUrl))
-.on("response", function (response) {
-res.setHeader("Access-Control-Allow-Origin", origin);
-})
-.pipe(res);
+  const targetUrl = `${BASE_URL}${req.originalUrl}`;
+  
+  req.pipe(request(targetUrl))
+    .on("response", function (response) {
+      res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+      res.setHeader("Content-Type", response.headers["content-type"]);
+    })
+    .pipe(res);
 });
 
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-console.log(Proxy listening on port ${PORT});
+  console.log(`Proxy listening on port ${PORT}`);
 });
