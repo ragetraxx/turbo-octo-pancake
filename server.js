0a40fa42c2773ecb7c5089aeb2f4aba2d39f27c3
ragetraxx <93855827+ragetraxx@users.noreply.github.com>
2025-08-28 20:49:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,44 +4,31 @@ const cors = require("cors");
 
 const app = express();
 const BASE_URL = "http://143.44.136.110:6610";
-const allowedOrigin = "https://ragetb.onrender.com";
 
-// Enable CORS for allowed origin or no origin (IPTV apps)
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || origin === allowedOrigin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  }
-}));
+// Allow all origins
+app.use(cors());
 
-// Manually set CORS headers for streaming
+// Manually set headers for streaming
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (!origin || origin === allowedOrigin) {
-    res.setHeader("Access-Control-Allow-Origin", origin || "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    next();
-  } else {
-    res.status(403).send("Access denied");
-  }
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
 });
 
 // Proxy all GET requests
 app.get("/*", (req, res) => {
   const targetUrl = `${BASE_URL}${req.originalUrl}`;
 
-  req.pipe(request(targetUrl))
+  req
+    .pipe(request(targetUrl))
     .on("response", function (response) {
-      res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+      res.setHeader("Access-Control-Allow-Origin", "*");
       res.setHeader("Content-Type", response.headers["content-type"]);
     })
     .pipe(res);
 });
 
-// Start the server
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Proxy listening on port ${PORT}`);
